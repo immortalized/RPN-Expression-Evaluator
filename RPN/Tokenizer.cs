d5197c9fb05af0a97ee93dd8fcf1cdcb79841644
immortalized <71087461+immortalized@users.noreply.github.com>
2024-06-05 22:39:47 +0200

    Update Tokenizer.cs

--- RPN/Tokenizer.cs
+++ RPN/Tokenizer.cs
@@ -6,7 +6,7 @@ public static class Tokenizer
     public static string[] Tokenize(string input)
     {
         List<string> tokens = new List<string>();
-        string pattern = @"(\d+(\.\d+)?)|sqrt|sin|cos|tan|asin|acos|atan|exp|log|lg|!|pi|e|[\+\-\*/\^\(\)]";
+        string pattern = @"(\d+(\.\d+)?)|sqrt|sinh|cosh|tanh|sin|cos|tan|exp|log|lg|!|pi|e|[\+\-\*/\^\(\)]";
         foreach (Match match in Regex.Matches(input, pattern))
         {
             tokens.Add(match.Value);
